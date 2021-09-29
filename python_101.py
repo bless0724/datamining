ee377eee7885028e693f39a8ebfaae505a983e6a
bless0724 <1318689423@qq.com>
2021-09-29 21:57:27 +0800

    继续作业1

--- python_101.py
+++ python_101.py
@@ -13,8 +13,9 @@ Created on Fri Jan 06 10:08:42 2017
 
 """
 #####################################################################
-
+import decorator
 import numpy as np
+import pandas as pd
 from scipy import signal
 import math
 import matplotlib
@@ -24,37 +25,44 @@ import os
 import gc   #gc模块提供一个接口给开发者设置垃圾回收的选项
 import time
 
-def correlation_coefficient(array1, array2, n):
-    sum1 = 0.0
-    sum2 = 0.0
-    sum11 = 0.0
-    sum22 = 0.0
-    sum12 = 0.0
-    for i in range(0, n):
-        sum1 += array1[i]
-        sum2 += array2[i]
-        sum12 += array1[i]*array2[i]
-        sum11 += math.pow(array1[i], 2)
-        sum22 += math.pow(array2[i], 2)
-    cov12 = (n*sum12-sum1*sum2)/math.pow(n, 2)
-    var1 = (n*sum11-math.pow(sum1, 2))/math.pow(n, 2)
-    var2 = (n*sum22-math.pow(sum2, 2))/math.pow(n, 2)
-    cor = cov12/math.sqrt(var1 * var2)
-    return cor
+# def correlation_coefficient(array1, array2, n): #计算相关系数
+#     sum1 = 0.0
+#     sum2 = 0.0
+#     sum11 = 0.0
+#     sum22 = 0.0
+#     sum12 = 0.0
+#     for i in range(0, n):
+#         sum1 += array1[i]
+#         sum2 += array2[i]
+#         sum12 += array1[i]*array2[i]
+#         sum11 += math.pow(array1[i], 2)
+#         sum22 += math.pow(array2[i], 2)
+#     cov12 = (n*sum12-sum1*sum2)/math.pow(n, 2)
+#     var1 = (n*sum11-math.pow(sum1, 2))/math.pow(n, 2)
+#     var2 = (n*sum22-math.pow(sum2, 2))/math.pow(n, 2)
+#     cor = cov12/math.sqrt(var1 * var2)
+#     return cor
+
+def list_of_groups(init_list, childern_list_len):#设定滑动时间窗，每一个sharp wave为一个list
+    list_of_groups = zip(*(iter(init_list),) *childern_list_len)
+    end_list = [list(i) for i in list_of_groups]
+    count = len(init_list) % childern_list_len
+    end_list.append(init_list[-count:]) if count !=0 else end_list
+    return end_list
+
 
 #读取文件第一列，保存在s1列表中
 ###########################################################################################################
 start = 113 #从start开始做N个文件的图                    #设立变量start，作为循环读取文件的起始
 N = 1                                                      #设立变量N，作为循环读取文件的增量
 for e in range(start,start+N):                            #循环2次，读取113&114两个文件
-    data = open(r'20151026_%d'% (e)).read()     #设立data列表变量，python 文件流，%d处，十进制替换为e值，.read读文件
+    data = open(r'20151026_%d' % (e)).read()     #设立data列表变量，python 文件流，%d处，十进制替换为e值，.read读文件
     data = data.split( )                                  #以空格为分隔符，返回数值列表data
     data = [float(s) for s in data]                       #将列表data中的数值强制转换为float类型
 
-    s1 = data[0:15000*4:4]                          #list切片L[n1:n2:n3]  n1代表开始元素下标；n2代表结束元素下标
+    s1 = data[0:14994*4:4]                          #list切片L[n1:n2:n3]  n1代表开始元素下标；n2代表结束元素下标
                                                     #n3代表切片步长，可以不提供，默认值是1，步长值不能为0
 ####################################################################################################################
-
 
 #滤波
 ##################################################################################################################
@@ -67,6 +75,31 @@ for e in range(start,start+N):                            #循环2次，读取11
     high = highcut/nyq
     b,a = signal.butter(order,[low,high],btype='band') #设计巴特沃斯带通滤波器 “band”
     s1_filter1 = signal.lfilter(b,a,s1)                 #将s1带入滤波器，滤波结果保存在s1_filter1中
+    #print(s1_filter1)
+###################################################################################################################
+
+#计算相关性与相异性
+###################################################################################################################
+    cor = []
+    #id = []
+    idcompare = []
+    wave = list_of_groups(s1_filter1 ,1666)#滑动时间窗设为1666ms
+    count = int(len(s1_filter1)/ 1666)#计算有多少个sharp_wave
+    # for i in range(0,count):
+    #     id.append(i+1)
+    # data1 = pd.DataFrame({'id': id, 'wave': wave})
+    # print(data1)
+    for i in range(0, count-1):
+        for j in range(i+1, count):
+            wave2 = pd.Series(wave[j])
+            wave1 = pd.Series(wave[i])
+            cor1 = math.fabs(wave1.corr(wave2))#计算两个sharp_wave的pearson相关系数
+            cor.append(cor1)#记录相关系数
+            idcompare.append([i+1,j+1])#记录两个波的id
+            #cor.append([math.fabs(correlation_coefficient(wave[i], wave[j], 1666)),[i+1,j+1]])
+    data2 = pd.DataFrame({'idcompare': idcompare, 'cor': cor})
+    data2.sort_values(by='cor', axis=0, ascending=True, inplace=True)#按照相关系数大小排序
+    print(data2)
 ###################################################################################################################
 
 #画图
@@ -90,7 +123,7 @@ for e in range(start,start+N):                            #循环2次，读取11
     #plt.subplots_adjust(hspace=1)
     #plt.savefig(r'20151026_%d.png' % (e))  #保存图像，设定保存路径并统一命名，%d处，十进制替换为e值
     #plt.close('all')                                 #关闭绘图对象，释放绘图资源
-    print(e)
+    #print(e)
 ##################################################################################################################
 
 

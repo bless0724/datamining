af09c63793e1bd82459515ac9062a205f64e3976
bless0724 <1318689423@qq.com>
2021-10-25 21:45:34 +0800

    作业4KNN完成

--- knn.py
+++ knn.py
@@ -1,14 +1,14 @@
 # -*- coding: utf-8 -*-     支持文件中出现中文字符
 #####################################################################
+import operator
 import random
+
+import numpy
 import numpy as np
 import pandas as pd
 from scipy import signal
 from sklearn.model_selection import train_test_split
-from sklearn.tree import DecisionTreeClassifier
 from sklearn.metrics import accuracy_score
-from sklearn import tree
-import graphviz
 
 def  psfeatureTime(data):#计算特征
     # 均值
@@ -39,6 +39,39 @@ def list_of_groups(init_list, childern_list_len):#设定滑动时间窗，每一
     count = len(init_list) % childern_list_len
     end_list.append(init_list[-count:]) if count != 0 else end_list
     return end_list
+
+
+def knn(trainData, testData, labels, k):
+    # 计算训练样本的行数
+    rowSize = trainData.shape[0]
+    # 计算训练样本和测试样本的差值
+    diff = np.tile(testData, (rowSize, 1)) - trainData
+    # 计算差值的平方和
+    sqrDiff = diff ** 2
+    sqrDiffSum = sqrDiff.sum(axis=1)
+    # 计算距离
+    distances = sqrDiffSum ** 0.5
+    # 对所得的距离从低到高进行排序
+    sortDistance = distances.argsort()
+
+    count = {}
+
+    for i in range(k):
+        vote = labels[sortDistance[i]]
+        count[vote] = count.get(vote, 0) + 1
+    # 对类别出现的频数从高到低进行排序
+    sortCount = sorted(count.items(), key=operator.itemgetter(1), reverse=True)
+
+    # 返回出现频数最高的类别
+    return sortCount[0][0]
+
+def confusion_matrix(predict,real):
+    matrix = numpy.zeros((3,3)).astype('int64')
+    for i in range(len(predict)):
+        x = int(predict[i])-1
+        y = int(real[i])-1
+        matrix[x][y] += 1
+    return matrix
 
 #读取文件第一列，保存在s1列表中
 ###########################################################################################################
@@ -78,17 +111,9 @@ for e in range(start, start+N):                            #循环2次，读取1
     features = np.array(s1_feature)
     labels = np.array(s1_label)
     #print(labels)
-    train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size=0.33, random_state=1)#划分训练集和测试集
-    clf = DecisionTreeClassifier(criterion='gini')#调用生成决策树分类器
-    clf = clf.fit(train_features, train_labels)
-    dot_data = tree.export_graphviz(clf, out_file=None,#可视化决策树
-                         feature_names=["均值","偏度","峰度","峰值因子","裕度因子","峭度"],
-                         class_names=["1","2","3"],
-                         filled=True, rounded=True,
-                         special_characters=True)
-    graph = graphviz.Source(dot_data)
-    graph.render("decision tree")#保存
-
-    test_predict = clf.predict(test_features)
-    score = accuracy_score(test_labels, test_predict)
-    print("分类树准确率 %.4lf" % score)
+    train_features, test_features, train_labels, test_labels = train_test_split(features, labels, test_size=0.33, random_state=0)#划分训练集和测试集
+    test_predict = []
+    for i in range(len(test_labels)):
+        X = knn(train_features, test_features[i], train_labels, 5)
+        test_predict.append(X)
+    print(confusion_matrix(test_predict, test_labels))
